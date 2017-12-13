97fecf9079109378838449d63f788120ce38d576
Goncalo Pereira <github@goncalopereira.com>
2017-12-13 10:57:06 +0000

    library upgrades

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -2,18 +2,23 @@ import sbt.Keys._
 import sbt._
 import uk.gov.hmrc.SbtAutoBuildPlugin
 import uk.gov.hmrc.versioning.SbtGitVersioning
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
 
 object HmrcBuild extends Build {
 
   val appName = "reference-checker"
+  lazy val plugins : Seq[Plugins] = Seq(play.sbt.PlayScala)
 
   lazy val microservice = Project(appName, file("."))
-    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
+    .enablePlugins(Seq(play.sbt.PlayScala,SbtAutoBuildPlugin, SbtDistributablesPlugin, SbtGitVersioning) ++ plugins : _*)
     .settings(
-      scalaVersion := "2.11.7",
+      scalaVersion := "2.11.11",
       libraryDependencies ++= BuildDependencies(),
-      crossScalaVersions := Seq("2.11.7")
+      crossScalaVersions := Seq("2.11.11")
     )
+    .settings(publishingSettings: _*)
+    .settings(SbtDistributablesPlugin.publishingSettings)
     .disablePlugins(sbt.plugins.JUnitXmlReportPlugin)
 
 }
@@ -21,9 +26,9 @@ object HmrcBuild extends Build {
 private object BuildDependencies {
 
   lazy val testDeps = Seq(
-    "org.scalatest" %% "scalatest" % "2.2.4" % "test",
-    "org.scalacheck" %% "scalacheck" % "1.12.2" % "test",
-    "org.pegdown" % "pegdown" % "1.5.0" % "test"
+    "org.scalatest" %% "scalatest" % "3.0.0" % "test",
+    "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
+    "org.pegdown" % "pegdown" % "1.6.0" % "test"
   )
 
   def apply() = testDeps
